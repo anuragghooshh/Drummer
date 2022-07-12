1e829b70a78e93313382d8b4a86b1a277f7cdbd2
Anurag Ghosh <82631826+anuragghooshh@users.noreply.github.com>
2022-07-12 21:36:13 +0530

    Add files via upload

--- Script/script.js
+++ Script/script.js
@@ -1,21 +1,27 @@
 const drums = document.querySelectorAll(".drums");
 const audios = [
-    new Audio('./Resources/Drumbasssinglekick.mp3'),
+    new Audio('./Resources/Plate.mp3'),
+    new Audio('./Resources/Drum1.mp3'),
+    new Audio('./Resources/Drum2.mp3'),
+    new Audio('./Resources/Drum3.mp3'),
+    new Audio('./Resources/Plate2.mp3'),
+    new Audio('./Resources/Pedal.mp3'),
     new Audio('./Resources/Snare2.mp3'),
     new Audio('./Resources/bongo.mp3'),
     new Audio('./Resources/Cymbal1.mp3'),
-    new Audio('./Resources/drumStick.mp3')
+    new Audio('./Resources/drumStick.mp3'),
 ];
-const keys = [1,2,3,4,5];
+const keys = [0,1,2,3,4,5,6,7,8,9];
 
-for(let i=0;i<5;i++){
+for(let i=0;i<10;i++){
     drums[i].addEventListener("click",()=>{
         const newAudio = audios[i].cloneNode();
         newAudio.play();
     })
+    drums[i].setAttribute('draggable',false);
 }
 document.addEventListener("keydown",e=>{
-    for(let j=0;j<5;j++){
+    for(let j=0;j<10;j++){
         if(e.key == keys[j]){
             const newAudio = audios[j].cloneNode();
             newAudio.play();
